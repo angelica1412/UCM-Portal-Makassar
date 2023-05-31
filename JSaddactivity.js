b7f9a8314a4109a247478c214521cb1df1a097b5
rigeltandilolo <ijengrigel@gmail.com>
2023-05-31 16:06:43 +0800

    komit
    komit ubahan form tanggal 31 mei

--- JSaddactivity.js
+++ JSaddactivity.js
@@ -5,3 +5,14 @@ input.onchange = (e) => {
     if (input.files[0])
     img.src = URL.createObjectURL(input.files[0])
 };
+
+function validateForm() {
+    var inputs = document.getElementsByTagName('input');
+    for(var i = 0; i < inputs.length; i++) {
+        if(inputs[i].value == '') {
+            inputs[i].classList.add('error');
+        } else {
+            inputs[i].classList.remove('error');
+        }
+    }
+}
