4dcb30653b18bd22819ea16bdf4ef1d862fb0789
angelica1412 <114383532+angelica1412@users.noreply.github.com>
2023-06-05 22:52:54 +0800

    bebann

--- js/JSaddactivity.js
+++ js/JSaddactivity.js
@@ -3,16 +3,34 @@ let input = document.getElementById('input');
 
 input.onchange = (e) => {
     if (input.files[0])
-    img.src = URL.createObjectURL(input.files[0])
+        img.src = URL.createObjectURL(input.files[0])
 };
 
 function validateForm() {
     var inputs = document.getElementsByTagName('input');
-    for(var i = 0; i < inputs.length; i++) {
-        if(inputs[i].value == '') {
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == '') {
             inputs[i].classList.add('error');
         } else {
             inputs[i].classList.remove('error');
         }
     }
 }
+
+$ = function (id) {
+    return document.getElementById(id);
+}
+
+var show = function (id) {
+    $(id).style.display = 'flex';
+}
+var hide = function (id) {
+    $(id).style.display = 'none';
+}
+
+window.addEventListener('click', function(event) {
+    var popup = document.getElementById('popup');
+    if (event.target == popup) {
+        hide('popup');
+    }
+});
