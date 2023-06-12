919a455166828d759e710365273d82758e616bc7
putrataufik <daengkadaeng@gmail.com>
2023-06-12 16:37:11 +0800

    admin
    perubahan admin, form, dengan home

---
+++ js/requestlist.js
@@ -0,0 +1,123 @@
+function getRequestList() {
+    var apiUrl = "http://localhost:4000/ucmportal/listrqform";
+  
+    // Lakukan request AJAX ke API menggunakan jQuery
+    $.ajax({
+      url: apiUrl,
+      type: "GET",
+      dataType: "json",
+      success: function(requestList) {
+        processRequestList(requestList);
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+      }
+    });
+  }
+  
+  function processRequestList(requestList) {
+    var getRequestList = document.getElementById("list_Pengirim");
+  
+    // Kosongkan kontainer
+    getRequestList.innerHTML = "";
+  
+    // Mengecek apakah ada data form terbaru
+    if (requestList.length > 0) {
+      requestList.forEach(function(list) {
+        // Membuat elemen HTML untuk form
+        var infopengirimDiv = document.createElement("button");
+        infopengirimDiv.className = "infopengirim";
+        infopengirimDiv.innerHTML = `
+          <div class="profile">
+            <span class="dotprofile"><i class="fa-solid fa-user"></i></span>
+          </div>
+          <div class="name">
+            <p class="namepengirim">${list.userName}</p>
+            <p class="jurusan">${list.nim} - ${list.major}</p>
+          </div>
+          <div class="iconbeside">
+            <a href="#"><i class="fa-solid fa-chevron-right"></i></a>
+          </div>
+        `;
+  
+        infopengirimDiv.addEventListener("click", function() {
+          var userName = list.userName;
+          var formTitle = list.formTitle;
+          showFormularData(userName, formTitle);
+        });
+  
+        // Tambahkan elemen form ke dalam kontainer
+        getRequestList.appendChild(infopengirimDiv);
+      });
+    } else {
+      getRequestList.innerHTML = "Tidak ada data form terbaru.";
+    }
+  }
+  
+  function showFormularData(userName, formTitle) {
+    var apiUrlForm = `http://localhost:4000/ucmportal/form/${formTitle}`;
+    console.log(userName);
+  
+    // Lakukan request AJAX ke API menggunakan jQuery
+    $.ajax({
+      url: apiUrlForm,
+      type: "GET",
+      dataType: "json",
+      success: function(formularData) {
+        console.log(formularData);
+        // Memperbarui tampilan HTML dengan data formular
+        var judulKegiatan = document.querySelector(".judulkegiatan");
+        var jenisKegiatan = document.querySelector(".jawabanjenis");
+        var tanggalMulai = document.querySelector(".tanggalmulai");
+        var tanggalAkhir = document.querySelector(".tanggalakhir");
+        var deskripsi = document.querySelector(".descinfo");
+        var logoKegiatan = document.querySelector(".logo1 img");
+        var linkGoogleForm = document.querySelector(".rectangle");
+        var tombolTolak = document.querySelector(".tolak");
+        var tombolTerima = document.querySelector(".acc");
+  
+        judulKegiatan.textContent = formularData[0].title;
+        jenisKegiatan.textContent = formularData[0].type;
+        tanggalMulai.textContent = "Mulai: " + formularData[0].dateStart;
+        tanggalAkhir.textContent = "Akhir: " + formularData[0].dateEnd;
+        deskripsi.innerHTML = formularData[0].description;
+        logoKegiatan.src = "data:image/jpeg;base64," + formularData[0].logo;
+        linkGoogleForm.textContent = formularData[0].linkGoogleForm;
+  
+        tombolTolak.addEventListener("click", function() {
+          updateFormStatus(formTitle, "rejected");
+        });
+  
+        tombolTerima.addEventListener("click", function() {
+          updateFormStatus(formTitle, "accepted");
+        });
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+      }
+    });
+  }
+  
+  function updateFormStatus(formTitle, status) {
+    var apiUrlUpdateForm = `http://localhost:4000/ucmportal/updateform/${formTitle}/${status}`;
+  
+    // Lakukan request AJAX ke API menggunakan jQuery
+    $.ajax({
+      url: apiUrlUpdateForm,
+      type: "PUT",
+      success: function(response) {
+        console.log(response);
+        // Tambahkan logika atau tindakan yang diinginkan setelah update status form berhasil
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+      }
+    });
+  }
+  
+  getRequestList();
+  
+  setInterval(function() {
+    getRequestList();
+  }, 5000);
+  
